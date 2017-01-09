52d42611eae91b8c703c173a39bbcc0e14196138
James <millerjt82@gmail.com>
2017-01-09 16:56:30 -0500

    changes

--- projects/random-quote-machine/scripts/script.js
+++ projects/random-quote-machine/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var quote = '';
+  var randomQuote = '';
 
   getQuote();
 
@@ -8,17 +8,9 @@ $(document).ready(function() {
       getQuote();
     });
 
-// twitter section
-
-/* $(".twitter").click(function() {
-   var tweet = "https://twitter.com/intent/tweet?text=" + getQuote + " - authur";
-   window.open(tweet, "twitter");
-   return false;
-*/ });
-
 function getQuote() {
 
-  var myQuotes = [
+  var quotation = [
               'I was adored once too, William Shakepeare',
               'Belief creates the actual fact, William James',
               'To fail to love is not to exist at all, Mark Van Doren',
@@ -47,10 +39,10 @@ function getQuote() {
               'Wine is constant proof that God loves us and loves to see us happy., Benjamin Franklin',
               'We are all born ignorant, but one must work hard to remain stupid., Benjamin Franklin',
               'We are what we believe we are., C.S. Lewis',
-              'Today you are you! That is truer than true! There is no one alive who is you-er than you!, Dr. Seuss'
-              'I like nonsense; it wakes up the brain cells., Dr. Seuss',
+              'Today you are you! That is truer than true! There is no one alive who is you-er than you!, Dr. Seuss',
+              'I like nonsense, it wakes up the brain cells., Dr. Seuss',
               'The more you read, the more things you will know. The more that you learn, the more places youll go., Dr. Seuss',
-              'Cock you hat - angles are attitudes., Frank Sinatra'
+              'Cock you hat - angles are attitudes., Frank Sinatra',
               'ALcohol may be a mans worst enemy, but the Bible says love your enemy., Frank Sinatra',
               'Progress is impossible without change, and those who cannot change their minds cannot change anything., George Bernard Shaw',
               'Just do what must be done. This may not be happiness, but it is greatness., George Bernard Shaw',
@@ -62,15 +54,15 @@ function getQuote() {
               'You must be the change you wish to see in the world., Mahatma Gandhi',
               'The secret of getting ahead is getting started., Mark Twain',
               'Get your facts first, then you can distort them as you please., Mark Twain',
-              'Don\'t let schooling interfere with your education., Mark Twain']
+              'Don\'t let schooling interfere with your education., Mark Twain',
               'Whenever you find yourself on the side of the majority, it is time to pause and reflect., Mark Twain',
               'A nickel ain\'t worth a dime anymore., Yogi Berra',
-              'Little League baseball is a very good thing becuase it keeps the parents off the streets., Yogi Berra'
-            ];
+              'Little League baseball is a very good thing becuase it keeps the parents off the streets., Yogi Berra'];
 
-            quote = getQuotes[Math.floor(Math.random() * myQuotes.length)];
+            randomQuote = quotation[Math.floor(Math.random() * quotation.length)];
 
-        }
+            $(".quotes").text(randomQuote);
 
-      });
+          }
 
+});
